fc3635e4562e544afdedc827db8cd768f0a87668
codekyng <teryima127@gmail.com>
2023-01-30 22:12:21 +0100

    first commit

--- main.go
+++ main.go
@@ -4,13 +4,26 @@ import (
     "context"
     "fmt"
     "github.com/ethereum/go-ethereum/ethclient"
-    "github.com/ethereum/go-ethereum/common/hexutil"
-    "github.com/ethereum/go-ethereum/crypto"
+    // "github.com/ethereum/go-ethereum/common/hexutil"
+    // "github.com/ethereum/go-ethereum/crypto"
     "log"
 )
 
 var (
     ctx         = context.Background()
-    url         = "Your Infura URL here"
+    url         = "https://mainnet.infura.io/v3/407f60619ec14e538991ba8f9e0f4237"
     client, err = ethclient.DialContext(ctx, url)
 )
+
+
+func currentBlock() {
+    block, err := client.BlockByNumber(ctx, nil)
+    if err != nil {
+        log.Println(err)
+    }
+    fmt.Println(block.Number())
+}
+
+func main() {
+	currentBlock()
+}
