f32b9fc82961c1576b2f049d9d6e3a9c0789e8bc
codekyng <teryima127@gmail.com>
2023-01-31 20:10:08 +0100

    create wallet

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
     "context"
     "fmt"
     "github.com/ethereum/go-ethereum/ethclient"
-    // "github.com/ethereum/go-ethereum/common/hexutil"
-    // "github.com/ethereum/go-ethereum/crypto"
+    "github.com/ethereum/go-ethereum/common/hexutil"
+    "github.com/ethereum/go-ethereum/crypto"
     "log"
 )
 
@@ -24,6 +24,34 @@ func currentBlock() {
     fmt.Println(block.Number())
 }
 
+func createWallet() (string, string) {
+    getPrivateKey, err := crypto.GenerateKey()
+
+    if err != nil {
+        log.Println(err)
+    }
+
+    getPublicKey := crypto.FromECDSA(getPrivateKey)
+    thePublicKey := hexutil.Encode(getPublicKey)
+
+}
+
+
+
 func main() {
 	currentBlock()
+
+    // Querying Ethereum wallet balances with Geth
+
+    address := common.HexToAddress("0x8335659d19e46e720e7894294630436501407c3e")
+
+    balance, err := client.BalanceAt(ctx, address, nil)
+    if err != nil {
+        log.Print("There was an error", err)
+    }
+    fmt.Println("The balance aof the provided wallet address is", balance)
+
+    pubAddress, pubKey := createWallet()
+
+    fmt.Printf("Public address:%s, public key: %s\n", pubAddress, pubKey)
 }
